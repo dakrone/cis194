f394307f080d2247c155fddf286267c8229d8c62
Lee Hinman <lee@writequit.org>
2017-05-19 16:09:02 -0600

    Add (a terrible) skips method for week3

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -2,5 +2,27 @@
 
 module Golf where
 
+-- Exercise 1
+
+-- Return every @x@ elements in the array
+every :: Int -> [a] -> [a]
+every _ [] = []
+every x yz =
+  let i = zip [1..] yz
+      filtered = filter (\t -> (fst t) `mod` x == 0) i
+  in map snd filtered
+
+-- First list should be the same as the input list, the second should contain
+-- every second element from the input list, with the nth list having every nth
+-- element
 skips :: [a] -> [[a]]
-skips = undefined
+skips l =
+  -- Create a `length` repeat of the list, with indices
+  let wo = zip [1..(length l)] (repeat l)
+      -- map the "every" method over it
+      we = map (\t -> every (fst t) (snd t)) wo
+  in we
+
+------------------------------
+
+-- Exercise 2
