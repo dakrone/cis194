5412b2f03782180543961299b17be407c280f10f
Lee Hinman <lee@writequit.org>
2017-05-23 16:26:24 -0600

    Start week5
    I may have cheated and used fmap

---
+++ week5/Calc.hs
@@ -0,0 +1,19 @@
+module Calc where
+
+import ExprT
+import Parser
+
+-- Ex01
+
+-- be able to eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
+
+-- Ex02
+
+evalStr :: String -> Maybe Integer
+evalStr s = fmap eval $ parseExp Lit Add Mul s
+
+-- Ex03
